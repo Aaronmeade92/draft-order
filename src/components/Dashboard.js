2789e414267a0c673cbdd86768c8042b2562d330
Aaron <aaronmeade9292@gmail.com>
2019-04-18 17:54:38 -0700

    got the list written and some array

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import shuffle from 'shuffle-array';
+
+const center = {
+    paddingLeft: '40%',
+    paddingTop: '10%',
+}
+
+const buttonCenter = {
+    textAlign: 'center',
+    display: 'inlineBlock',
+}
+
 
 class Dashboard extends Component {
+    state = {
+        query: '',
+        draftOrder: [],
+    };
+
+    
+
+    addToList = () => {
+        let ul = document.getElementById('list');
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(this.state.query));
+        ul.appendChild(li);
+        this.state.draftOrder.push(this.state.query);
+        console.log(this.state.draftOrder);
+    }
+
+    randomizeOrder = () => {
+        shuffle(this.state.draftOrder);
+        console.log(this.state.draftOrder);
+    }
+
+    handleInputChange = () => {
+        this.setState({
+            query: this.search.value
+        });
+    };
+
     render() {
         return (
-            <div>
-                <h1>React-Seed</h1>
-            </div>
+            <Fragment>
+                <div style={center}>
+                    <h1>Draft Order</h1>
+                    <form>
+                        <input placeholder='Add to your draft...'
+                        ref={input => this.search = input}
+                        onChange={this.handleInputChange}
+                        />
+                    </form>
+                </div>
+                <div style={buttonCenter}>
+                    <button onClick={this.addToList} style={buttonCenter}>Add</button>
+                    <button onClick={this.randomizeOrder} style={buttonCenter}>Randomize</button>
+                </div>
+
+                <div style={center}>
+                    <ul id='list'>Draft Order</ul>
+                </div>
+
+            </Fragment>
         );
     }
 }
